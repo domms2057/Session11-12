18abb5b488d32deab2242b216cbfc43a044ef035
dommz <domiziano.myers@gmail.com>
2025-02-14 11:37:51 +0100

    Initial commit

---
+++ Sess11/12.py
@@ -0,0 +1,16 @@
+
+# find 3 letter words starting with b inside file
+def find_words(filename):
+    """
+    Prints 3 letter words starting with b from a file
+    :param input:
+    :return:
+    """
+    with open(filename) as f:
+        for line in f:
+            words = line.split()
+            for word in words:
+                if len(word) == 3 and word [0] in "bB":
+                    print(word)
+
+find_words("input")
